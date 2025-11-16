a5a0354b5fd42545780c973bc8ee7ae6f3d355dd
Zakopi <jiri.zakopal@gmail.com>
2025-11-16 15:57:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/bs-fyzio/" : "/",
+  base: mode === "production" ? "/bsfyzio-page-magic/" : "/",
   server: {
     host: "::",
     port: 8080,
